7c88058076fa37f5ec5ce7342db38138892ab44c
vinoca <vinoca@vinoca.org>
2020-06-01 14:36:30 +0800

    replace tokio to smol

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,33 @@ mod server;
 
 fn main() {
     let matches = clap::app_from_crate!()
-        .arg(Arg::with_name("listen")
-            .short("l")
-            .long("listen")
-            .takes_value(true)
-            .help("listen address"))
-        .arg(Arg::with_name("socks5")
-            .short("s")
-            .long("socks5")
-            .takes_value(true)
-            .help("socks5 server address"))
-        .arg(Arg::with_name("default")
-            .short("d")
-            .long("default")
-            .takes_value(true)
-            .help("default target address"))
+        .arg(
+            Arg::with_name("listen")
+                .short("l")
+                .long("listen")
+                .takes_value(true)
+                .help("listen address"),
+        )
+        .arg(
+            Arg::with_name("socks5")
+                .short("s")
+                .long("socks5")
+                .takes_value(true)
+                .help("socks5 server address"),
+        )
+        .arg(
+            Arg::with_name("default")
+                .short("d")
+                .long("default")
+                .takes_value(true)
+                .help("default target address"),
+        )
         .get_matches();
     let listen = matches.value_of("listen").unwrap_or("127.0.0.1:10800");
     let socks5 = matches.value_of("socks5").unwrap_or("127.0.0.1:1080");
     let default = matches.value_of("default").unwrap_or("1.1.1.1:53");
 
-    let mut rt = match tokio::runtime::Runtime::new() {
-        Ok(rt) => rt,
-        Err(e) => return println!("tokio runtime init error: {}", e),
-    };
-    if let Err(e) = rt.block_on(server::Server::run(&listen, &socks5, &default)) {
+    if let Err(e) = server::Server::run(&listen, &socks5, &default) {
         println!("startup error: {}", e)
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,18 +1,17 @@
 use std::{
     mem,
     net::{SocketAddr, SocketAddrV4},
+    net::{TcpListener, TcpStream},
     os::unix::io::AsRawFd,
 };
 
-use failure::{format_err, Fallible};
+use anyhow::{anyhow, Result};
+use futures::io::{copy, AsyncReadExt};
+use smol::{Async, Task};
 use socks5::connect_without_auth;
-use tokio::{
-    io::{copy, ErrorKind},
-    net::{lookup_host, TcpListener, TcpStream},
-};
 
 pub struct Server {
-    client: TcpStream,
+    client: Async<TcpStream>,
     listen: SocketAddr,
     server: SocketAddr,
     default_target_addr: SocketAddr,
@@ -21,7 +20,7 @@ pub struct Server {
 impl Server {
     pub fn new(
         listen: SocketAddr,
-        client: TcpStream,
+        client: Async<TcpStream>,
         server: SocketAddr,
         default_target_addr: SocketAddr,
     ) -> Self {
@@ -33,46 +32,29 @@ impl Server {
         }
     }
 
-    pub async fn run(
+    pub fn run(
         listen: &str,
         socks5_server_addr: &str,
         default_target_addr: &str,
-    ) -> Fallible<()> {
-        let listen = lookup_host(listen).await?.next().ok_or_else(|| {
-            let e: std::io::Error = ErrorKind::AddrNotAvailable.into();
-            e
-        })?;
-        let socks5_server_addr =
-            lookup_host(socks5_server_addr)
-                .await?
-                .next()
-                .ok_or_else(|| {
-                    let e: std::io::Error = ErrorKind::AddrNotAvailable.into();
-                    e
-                })?;
-        let default_target_addr =
-            lookup_host(default_target_addr)
-                .await?
-                .next()
-                .ok_or_else(|| {
-                    let e: std::io::Error = ErrorKind::AddrNotAvailable.into();
-                    e
-                })?;
-        let mut listener = TcpListener::bind(&listen).await?;
-
-        loop {
-            let (stream, _) = listener.accept().await?;
-            tokio::spawn(async move {
-                let mut server =
-                    Server::new(listen, stream, socks5_server_addr, default_target_addr);
-                if let Err(e) = server.proxy().await {
-                    println!("error: {}", e);
-                }
-            });
-        }
+    ) -> Result<()> {
+        let listen = listen.parse()?;
+        let socks5_server_addr = socks5_server_addr.parse()?;
+        let default_target_addr = default_target_addr.parse()?;
+        smol::run(async {
+            let listener = Async::<TcpListener>::bind(&listen)?;
+            loop {
+                let (stream, _) = listener.accept().await?;
+                Task::spawn(async move {
+                    let server = Server::new(listen, stream, socks5_server_addr, default_target_addr);
+                    if let Err(e) = server.proxy().await {
+                        println!("error: {}", e);
+                    }
+                }).detach();
+            }
+        })
     }
 
-    async fn proxy(&mut self) -> Fallible<()> {
+    async fn proxy(self) -> Result<()> {
         let dest_addr = match self.get_dest_addr() {
             Ok(addr) => {
                 if addr == self.listen {
@@ -83,14 +65,14 @@ impl Server {
             }
             Err(_) => self.default_target_addr,
         };
-        let mut srv = connect_without_auth(self.server, dest_addr).await?;
+        let srv = connect_without_auth(self.server, dest_addr).await?;
         let (mut srv_r, mut srv_w) = srv.split();
         let (mut r, mut w) = self.client.split();
         futures::future::select(copy(&mut r, &mut srv_w), copy(&mut srv_r, &mut w)).await;
         Ok(())
     }
 
-    fn get_dest_addr(&self) -> Fallible<SocketAddr> {
+    fn get_dest_addr(&self) -> Result<SocketAddr> {
         use libc::{__errno_location, getsockopt, sockaddr_in, socklen_t, SOL_IP, SO_ORIGINAL_DST};
         use std::ffi::c_void;
 
@@ -107,7 +89,7 @@ impl Server {
             );
             if r == -1 {
                 let errno = *__errno_location() as i32;
-                Err(format_err!("get original address error, errno: {}", errno))
+                Err(anyhow!("get original address error, errno: {}", errno))
             } else {
                 let addr = SocketAddrV4::new(
                     u32::from_be(destaddr.sin_addr.s_addr).into(),
